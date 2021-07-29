b787237db320ff202540d166f713833577b90026
JarTOD <87218254+JarTOD@users.noreply.github.com>
2021-07-29 16:28:38 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+///Not decode this script!!!
+///Susah bikinnya oi...
+return [
+    'currency' => 'DOGE',
+    'wallets' => [
+    ///Masukkan Wallet Doge faucetpay.io kalian///
+        'DAhJaqhE94MNBuQmSBbGVCfc9QajL82EVo',
+        
+        ]
+  ];
